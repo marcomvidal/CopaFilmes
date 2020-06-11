de1ef10e2bb80d6a48609e661b18a067451095c2
Marco M. Vidal <marcomvidal@gmail.com>
2020-06-11 12:43:18 -0300

    Front-end and back-end communication working

--- CopaFilmes.Frontend/src/app/services/film.service.ts
+++ CopaFilmes.Frontend/src/app/services/film.service.ts
@@ -2,15 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Film } from '../models/Film';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmService {
 
-  constructor(private http: HttpClient) { }
+  private url: string;
+
+  constructor(private http: HttpClient) {
+    this.url = `${environment.backendUrl}/films`;
+  }
 
   getAll(): Observable<Film[]> {
-    return this.http.get<Film[]>('https://localhost:5001/api/films');
+    return this.http.get<Film[]>(this.url).pipe(take(1));
   }
 }
